dda7441c6c2e6340c7989be3f630e48bee9d2104
bitha121295 <abyalax18@gmail.com>
2016-04-26 20:25:06 -0600

    Inicia subida de archivos

---
+++ eje2.py
@@ -0,0 +1,77 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -* 
+
+###########################################################################
+## Python code generated with wxFormBuilder (version Jun 17 2015)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO "NOT" EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class MyFrame1
+###########################################################################
+
+class MyFrame1 ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		fgSizer1 = wx.FlexGridSizer( 2, 2, 0, 0 )
+		fgSizer1.SetFlexibleDirection( wx.BOTH )
+		fgSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+		
+		fgSizer1.SetMinSize( wx.Size( 200,300 ) ) 
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Numero 1:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText1, 0, wx.ALL, 5 )
+		
+		self.txt_Num1 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.txt_Num1.SetMaxLength( 0 ) 
+		fgSizer1.Add( self.txt_Num1, 0, wx.ALL, 5 )
+		
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Numero 2:", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
+		fgSizer1.Add( self.m_staticText2, 0, wx.ALL, 5 )
+		
+		self.txt_Num2 = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.txt_Num2.SetMaxLength( 0 ) 
+		fgSizer1.Add( self.txt_Num2, 0, wx.ALL, 5 )
+		
+		
+		fgSizer1.AddSpacer( ( 0, 0), 1, wx.EXPAND, 5 )
+		
+		self.txt_Result = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.txt_Result.SetMaxLength( 0 ) 
+		fgSizer1.Add( self.txt_Result, 0, wx.ALL, 5 )
+		
+		self.m_button1 = wx.Button( self, wx.ID_ANY, u"Sumar", wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.m_button1, 0, wx.ALL, 5 )
+		
+		self.m_button2 = wx.Button( self, wx.ID_ANY, u"Salir", wx.DefaultPosition, wx.DefaultSize, 0 )
+		fgSizer1.Add( self.m_button2, 0, wx.ALL, 5 )
+		
+		
+		self.SetSizer( fgSizer1 )
+		self.Layout()
+	
+	def __del__( self ):
+		pass
+	
+###########################################################################
+		 
+class MyApp(wx.App):
+	def OnInit(self):
+		frame_1 = MyFrame1(None)
+		self.SetTopWindow(frame_1)
+		frame_1.Show()
+		return 1
+app = MyApp(0)
+app.MainLoop()
+	
+
